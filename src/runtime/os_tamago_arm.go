dad69a7d97822cf629bacc7fd12319f7a8f073ee
Andrej Rosano <andrej.rosano@f-secure.com>
2021-10-03 11:48:07 +0200

    runtime: move ARM initialization to application
    Move GOOS=tamago MMU initialization and exception handling to linked
    application.

--- src/runtime/os_tamago_arm.go
+++ src/runtime/os_tamago_arm.go
@@ -10,40 +10,7 @@ import (
 	"unsafe"
 )
 
-const tamagoDebug = true
-
 const _PAGESIZE uintptr = 0x1000
-
-// Memory region attributes
-// Table B3-10 ARM Architecture Reference Manual ARMv7-A and ARMv7-R edition
-const (
-	TTE_SECTION_1MB   uint32 = 0x2
-	TTE_SECTION_16MB  uint32 = 0x40002
-	TTE_EXECUTE_NEVER uint32 = 0x10
-	TTE_CACHEABLE     uint32 = 0x8
-	TTE_BUFFERABLE    uint32 = 0x4
-)
-
-// MMU access permissions
-// Table B3-8 ARM Architecture Reference Manual ARMv7-A and ARMv7-R edition
-const (
-	// PL1: no access   PL0: no access
-	TTE_AP_000 uint32 = 0b000000 << 10
-	// PL1: read/write  PL0: no access
-	TTE_AP_001 uint32 = 0b000001 << 10
-	// PL1: read/write  PL0: read only
-	TTE_AP_010 uint32 = 0b000010 << 10
-	// PL1: read/write  PL0: read/write
-	TTE_AP_011 uint32 = 0b000011 << 10
-	// Reserved
-	TTE_AP_100 uint32 = 0b100000 << 10
-	// PL1: read only   PL0: no access
-	TTE_AP_101 uint32 = 0b100001 << 10
-	// PL1: read only   PL0: read only
-	TTE_AP_110 uint32 = 0b100010 << 10
-	// PL1: read only   PL0: read only
-	TTE_AP_111 uint32 = 0b100011 << 10
-)
 
 // the following variables must be provided externally
 var ramStart uint32
@@ -56,7 +23,6 @@ var ramStackOffset uint32
 // the following functions must be provided externally
 func hwinit()
 func printk(byte)
-func exceptionHandler()
 func getRandomData([]byte)
 func initRNG()
 
@@ -64,86 +30,51 @@ func initRNG()
 // (but are already stubbed somewhere else in the runtime)
 //func nanotime1() int64
 
-// the following functions are defined in sys_tamago_arm.s
-func set_vbar(addr unsafe.Pointer)
-func set_ttbr0(addr unsafe.Pointer)
-func set_exc_stack(addr unsafe.Pointer)
+// GetRandomData generates len(r) random bytes from the random source provided
+// externally by the linked application.
+func GetRandomData(r []byte) {
+	getRandomData(r)
+}
 
-//go:nosplit
-func dmb()
-func semihostingstop()
+// CallOnG0 calls a function (func(off int)) on g0 stack.
+//
+// The function arguments must be passed through the following registers
+// (rather than on the frame pointer):
+//
+//   * R0: fn argument (vector table offset)
+//   * R1: fn pointer
+//   * R2: size of stack area reserved for caller registers
+//   * R3: caller program counter
+func CallOnG0()
+
+// MemRegion returns the start and end addresses of the physical RAM assigned
+// to the Go runtime.
+func MemRegion() (start uint32, end uint32) {
+	return ramStart, ramStart + ramSize
+}
 
 // stubs for unused/unimplemented functionality
 type mOS struct{}
 type sigset struct{}
 type gsignalStack struct{}
 
-func goenvs()                                             {}
-func msigsave(mp *m)                                      {}
-func msigrestore(sigmask sigset)                          {}
-func clearSignalHandlers()                                {}
-func sigblock()                                           {}
-func minit()                                              {}
-func unminit()                                            {}
-func madvise(addr unsafe.Pointer, n uintptr, flags int32) {}
-func munmap(addr unsafe.Pointer, n uintptr)               {}
-func setProcessCPUProfiler(hz int32)                      {}
-func setThreadCPUProfiler(hz int32)                       {}
-func initsig(preinit bool)                                {}
-func sigdisable(uint32)                                   {}
-func sigenable(uint32)                                    {}
-func sigignore(uint32)                                    {}
-func closeonexec(int32)                                   {}
-func osyield()                                            {}
-
-// global variables
-var vt *vector_table
-
-var vecTableOffset uint32 = 0
-var vecTableSize uint32 = 0x4000 // 16 kB
-
-var l1pageTableOffset uint32 = 0x4000 // 16 kB
-var l1pageTableSize uint32 = 0x4000   // 16 kB
-
-var excStackOffset uint32 = 0x8000 // 32 kB
-var excStackSize uint32 = 0x4000   // 16 kB
-
-// the following variables are set in sys_tamago_arm.s
-var stackBottom uint32
-
-// Table 11-1 ARM® Cortex™ -A Series Programmer’s Guide
-type vector_table struct {
-	// jump entries
-	reset          uint32
-	undefined      uint32
-	svc            uint32
-	prefetch_abort uint32
-	data_abort     uint32
-	_unused        uint32
-	irq            uint32
-	fiq            uint32
-	// call pointers
-	reset_addr          uint32
-	undefined_addr      uint32
-	svc_addr            uint32
-	prefetch_abort_addr uint32
-	data_abort_addr     uint32
-	_unused_addr        uint32
-	irq_addr            uint32
-	fiq_addr            uint32
-}
+func goenvs()                        {}
+func sigsave(p *sigset)              {}
+func msigrestore(sigmask sigset)     {}
+func clearSignalHandlers()           {}
+func sigblock(exiting bool)          {}
+func minit()                         {}
+func unminit()                       {}
+func mdestroy(mp *m)                 {}
+func setProcessCPUProfiler(hz int32) {}
+func setThreadCPUProfiler(hz int32)  {}
+func initsig(preinit bool)           {}
+func osyield()                       {}
 
 // May run with m.p==nil, so write barriers are not allowed.
 //go:nowritebarrier
 func newosproc(mp *m) {
 	panic("newosproc: not implemented")
-}
-
-func Setncpu(n int32) {
-	if n > 1 {
-		throw("unsupported: ncpu >= 2")
-	}
-	ncpu = n
 }
 
 // Called to do synchronous initialization of Go code built with
@@ -163,18 +94,13 @@ func mpreinit(mp *m) {
 }
 
 func osinit() {
-	// the kernel uses Setncpu() to update ncpu to the number of
-	// booted CPUs on startup
 	ncpu = 1
 	physPageSize = 4096
 	initBloc()
 }
 
 func signame(sig uint32) string {
-	if sig >= uint32(len(sigtable)) {
-		return ""
-	}
-	return sigtable[sig].name
+	return ""
 }
 
 func checkgoarm() {
@@ -204,142 +130,6 @@ func os_sigpipe() {
 //go:nosplit
 func crash() {
 	*(*int32)(nil) = 0
-}
-
-func GetRandomData(r []byte) {
-	getRandomData(r)
-}
-
-// the following functions are defined in sys_tamago_arm.s
-func resetHandler()
-func undefinedHandler()
-func svcHandler()
-func prefetchAbortHandler()
-func dataAbortHandler()
-func irqHandler()
-func fiqHandler()
-
-func fnAddress(fn func()) uint32 {
-	return **((**uint32)(unsafe.Pointer(&fn)))
-}
-
-//go:nosplit
-func vecinit() {
-	// Allocate the vector table
-	vecTableStart := ramStart + vecTableOffset
-	memclrNoHeapPointers(unsafe.Pointer(uintptr(vecTableStart)), uintptr(vecTableSize))
-	dmb()
-
-	// ldr pc, [pc, #24]
-	resetVectorWord := uint32(0xe59ff018)
-
-	vt = (*vector_table)(unsafe.Pointer(uintptr(vecTableStart)))
-	vt.reset = resetVectorWord
-	vt.undefined = resetVectorWord
-	vt.svc = resetVectorWord
-	vt.prefetch_abort = resetVectorWord
-	vt.data_abort = resetVectorWord
-	vt._unused = resetVectorWord
-	vt.irq = resetVectorWord
-	vt.fiq = resetVectorWord
-
-	vt.reset_addr = fnAddress(resetHandler)
-	vt.undefined_addr = fnAddress(undefinedHandler)
-	vt.svc_addr = fnAddress(svcHandler)
-	vt.prefetch_abort_addr = fnAddress(prefetchAbortHandler)
-	vt.data_abort_addr = fnAddress(dataAbortHandler)
-	vt.irq_addr = fnAddress(irqHandler)
-	vt.fiq_addr = fnAddress(fiqHandler)
-
-	if tamagoDebug {
-		print("vecTableStart    ", hex(vecTableStart), "\n")
-		print("vecTableSize     ", hex(vecTableSize), "\n")
-	}
-
-	set_vbar(unsafe.Pointer(vt))
-}
-
-//go:nosplit
-func excstackinit() {
-	// Allocate stack pointer for exception modes to provide a stack to the
-	// g0 goroutine when summoned by exception vectors.
-
-	excStackStart := ramStart + excStackOffset
-	memclrNoHeapPointers(unsafe.Pointer(uintptr(excStackStart)), uintptr(excStackSize))
-	dmb()
-
-	if tamagoDebug {
-		print("excStackStart    ", hex(excStackStart), "\n")
-		print("excStackSize     ", hex(excStackSize), "\n")
-		print("stackBottom      ", hex(stackBottom), "\n")
-		print("g0.stackguard0   ", hex(g0.stackguard0), "\n")
-		print("g0.stackguard1   ", hex(g0.stackguard1), "\n")
-		print("g0.stack.lo      ", hex(g0.stack.lo), "\n")
-		print("g0.stack.hi      ", hex(g0.stack.hi), "\n")
-		print("-- ELF image layout (firstmoduledata dump) --\n")
-		print(".text            ", hex(firstmoduledata.text), " - ", hex(firstmoduledata.etext), "\n")
-		print(".noptrdata       ", hex(firstmoduledata.noptrdata), " - ", hex(firstmoduledata.enoptrdata), "\n")
-		print(".data            ", hex(firstmoduledata.data), " - ", hex(firstmoduledata.edata), "\n")
-		print(".bss             ", hex(firstmoduledata.bss), " - ", hex(firstmoduledata.ebss), "\n")
-		print(".noptrbss        ", hex(firstmoduledata.noptrbss), " - ", hex(firstmoduledata.enoptrbss), "\n")
-		print(".end             ", hex(firstmoduledata.end), "\n")
-
-		imageSize := uint32(firstmoduledata.end - firstmoduledata.text)
-		heapSize := uint32(g0.stack.lo - firstmoduledata.end)
-		stackSize := uint32(g0.stack.hi - g0.stack.lo)
-		unusedSize := uint32(firstmoduledata.text) - (excStackStart + excStackSize) + ramStackOffset
-
-		print("-- Memory section sizes ---------------------\n")
-		print("vector table:    ", vecTableSize, " (", vecTableSize/1024, " kB)\n")
-		print("L1 page table:   ", l1pageTableSize, " (", l1pageTableSize/1024, " kB)\n")
-		print("exception stack: ", excStackSize, " (", excStackSize/1024, " kB)\n")
-		print("program image:   ", imageSize, " (", imageSize/1024, " kB)\n")
-		print("heap:            ", heapSize, " (", heapSize/1024, " kB)\n")
-		print("g0 stack:        ", stackSize, " (", stackSize/1024, " kB)\n")
-		print("total unused:    ", unusedSize, " (", unusedSize/1024, " kB)\n")
-
-		totalSize := vecTableSize + l1pageTableSize + excStackSize + imageSize + heapSize + stackSize + unusedSize
-		print("total:           ", totalSize, " (", totalSize/1024, " kB)\n")
-		print("---------------------------------------------\n")
-	}
-
-	set_exc_stack(unsafe.Pointer(uintptr(excStackStart + excStackSize)))
-}
-
-//go:nosplit
-func mmuinit() {
-	// Initialize page tables and map regions in privileged system area.
-	//
-	// MMU initialization is required to take advantage of data cache.
-	// http://infocenter.arm.com/help/index.jsp?topic=/com.arm.doc.faqs/ka13835.html
-
-	// Define a flat L1 page table, the MMU is enabled only for caching to work.
-	// The L1 page table is located 16KB after ramStart.
-
-	l1pageTableStart := ramStart + l1pageTableOffset
-	memclrNoHeapPointers(unsafe.Pointer(uintptr(l1pageTableStart)), uintptr(l1pageTableSize))
-
-	var i uint32
-
-	memAttr := uint32(TTE_AP_011 | TTE_CACHEABLE | TTE_BUFFERABLE | TTE_SECTION_1MB)
-	devAttr := uint32(TTE_AP_011 | TTE_SECTION_1MB)
-
-	for i = 0; i < l1pageTableSize/4; i++ {
-		if i >= (ramStart>>20) && i < ((ramStart+ramSize)>>20) {
-			*(*uint32)(unsafe.Pointer(uintptr(l1pageTableStart + 4*i))) = (i << 20) | memAttr
-		} else {
-			*(*uint32)(unsafe.Pointer(uintptr(l1pageTableStart + 4*i))) = (i << 20) | devAttr
-		}
-	}
-
-	dmb()
-
-	if tamagoDebug {
-		print("l1pageTableStart ", hex(l1pageTableStart), "\n")
-		print("l1pageTableSize  ", hex(l1pageTableSize), "\n")
-	}
-
-	set_ttbr0(unsafe.Pointer(uintptr(l1pageTableStart)))
 }
 
 //go:linkname syscall
@@ -397,8 +187,9 @@ func usleep(us uint32) {
 
 func exit(code int32) {
 	print("exit with code ", code, " halting\n")
-	// TODO: valid only within `qemu -semihosting`, support native hardware
-	semihostingstop()
+	for {
+		// hang forever
+	}
 }
 
 func exitThread(wait *uint32) {
@@ -409,7 +200,5 @@ func exitThread(wait *uint32) {
 const preemptMSupported = false
 
 func preemptM(mp *m) {
-	// Not currently supported.
-	//
-	// TODO: Use a note like we use signals on POSIX OSes
+	// No threads, so nothing to do.
 }
--- src/runtime/rt0_tamago_arm.s
+++ src/runtime/rt0_tamago_arm.s
@@ -5,22 +5,29 @@
 #include "textflag.h"
 
 TEXT _rt0_arm_tamago(SB),NOSPLIT,$0
-	// Raspberry Pi firmware sets CPU to HYP mode.
-	// Detect if in HYP mode and switch out of HYP mode
-	// to SVC mode (borrow technique from Linux kernel).
+	// Enforce little-endian operation
+	WORD	$0xf1010000	// setend le
+
+	// Detect HYP mode and switch to SVC if necessary
 	WORD	$0xe10f0000	// mrs r0, CPSR
-	EOR	$0x1a, R0	// 0x1a = HYP mode
-	TST	$0x1f, R0
-	BNE	after_eret	// Skip ERET if not HYP mode
+	AND	$0x1f, R0, R0	// get processor mode
+
+	CMP	$0x10, R0	// USR mode
+	B.EQ	runtime_start	// Skip initialization if USER mode
+
+	CMP	$0x1a, R0	// HYP mode
+	B.NE	after_eret	// Skip ERET if not HYP mode
+
 	BIC	$0x1f, R0
-	ORR	$0x1d3, R0	// 0x1d3 = AIF masked, SVC mode
+	ORR	$0x1d3, R0	// AIF masked, SVC mode
 	MOVW	$12(R15), R14	// add lr, pc, #12 (after_eret)
 	WORD	$0xe16ff000	// msr SPSR_fsxc, r0
 	WORD	$0xe12ef30e	// msr ELR_hyp, lr
 	WORD	$0xe160006e	// eret
-after_eret:
 
-	// Disable MMU as soon as possible. Will be re-enabled in mmuinit().
+after_eret:
+	// Disable MMU as soon as possible. It must be re-enabled by the
+	// application.
 	MRC	15, 0, R0, C1, C0, 0
 	BIC	$0x1, R0
 	MCR	15, 0, R0, C1, C0, 0
@@ -28,6 +35,7 @@ after_eret:
 	// Enter System Mode
 	WORD	$0xe321f0df	// msr CPSR_c, 0xdf
 
+runtime_start:
 	MOVW	runtime·ramStart(SB), R13
 	MOVW	runtime·ramSize(SB), R1
 	MOVW	runtime·ramStackOffset(SB), R2
